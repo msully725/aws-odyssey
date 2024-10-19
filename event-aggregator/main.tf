990000556f1c4f78a367c2be6634e142a861a04b
sully <sully@sullivandigitalconsulting.com>
2024-10-19 12:58:35 -0400

    Specify execution_arn when invoking lambda

--- event-aggregator/main.tf
+++ event-aggregator/main.tf
@@ -192,5 +192,6 @@ resource "aws_lambda_permission" "api_gateway_invoke" {
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.data_producer_lambda.function_name
     principal = "apigateway.amazonaws.com"
+    source_arn = "${aws_api_gateway_rest_api.event_api_gateway.execution_arn}/*/*"
 }
 
