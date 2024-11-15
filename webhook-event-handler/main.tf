a8eed0347ae8d50d1a564c7b61fc88ebc50b48f4
sully <sully@sullivandigitalconsulting.com>
2024-11-14 19:49:31 -0500

    Add API Gateway to SQS permissions

--- webhook-event-handler/main.tf
+++ webhook-event-handler/main.tf
@@ -26,3 +26,42 @@ resource "aws_sqs_queue" "webhook_event_queue" {
     visibility_timeout_seconds = 300 # 5 minutes
     message_retention_seconds = 86400 # 1 day
 }
+
+# API Gateway to SQS Integration
+resource "aws_iam_policy" "api_gateway_sqs_policy" {
+    name = "api-gateway-sqs-policy"
+    description = "API Gateway policy to enqueue in SQS"
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sqs:SendMessage"
+                Effect = "Allow"
+                Resource = aws_sqs_queue.webhook_event_queue.arn
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role" "api_gateway_role" {
+    name = "api-gateway-sqs-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "apigateway.amazonaws.com"
+                }
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_sqs_policy_attachment" {
+    role = aws_iam_role.api_gateway_role.name
+    policy_arn = aws_iam_policy.api_gateway_sqs_policy.arn
+}
