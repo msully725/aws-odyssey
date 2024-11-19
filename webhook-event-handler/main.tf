647935645a96c02c31147845534eafac7badef85
sully <sully@sullivandigitalconsulting.com>
2024-11-19 14:55:56 -0500

    Setting up an ECS task to pull from SQS

--- webhook-event-handler/main.tf
+++ webhook-event-handler/main.tf
@@ -169,6 +169,118 @@ resource "aws_api_gateway_integration_response" "sqs_200_response" {
   }
 }
 
+# VPC (for ECS)
+resource "aws_vpc" "webhook_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "webhook-vpc"
+  }
+}
+
+resource "aws_subnet" "webhook_subnet" {
+  count                  = 2
+  vpc_id                 = aws_vpc.webhook_vpc.id
+  cidr_block             = cidrsubnet(aws_vpc.webhook_vpc.cidr_block, 8, count.index)
+  availability_zone      = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "webhook-subnet-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_security_group" "ecs_task_sg" {
+  name   = "ecs-task-sg"
+  vpc_id = aws_vpc.webhook_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS 
+resource "aws_ecs_cluster" "webhook_event_handler_cluster" {
+  name = "webhook-event-handler-cluster"
+}
+
+resource "aws_ecr_repository" "webhook_event_handler_repo" {
+  name = "webhook-event-handler-repo"
+}
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "webhook_event_handler_task_definition" {
+  family = "sqs-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  memory = "512"
+  cpu = "256"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name        = "event-handler",
+      image       = "${aws_ecr_repository.webhook_event_handler_repo.repository_url}:latest",
+      essential   = true,
+      environment = [
+        {
+          name  = "SQS_QUEUE_URL",
+          value = aws_sqs_queue.webhook_event_queue.url
+        }
+      ],
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-region = var.region
+          awslogs-group = "/ecs/webhook-event-handler-task"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "webhook_event_handler_service" {
+  name = "webhook-event-handler-service"
+  cluster = aws_ecs_cluster.webhook_event_handler_cluster.id
+  task_definition = aws_ecs_task_definition.webhook_event_handler_task_definition.arn
+  launch_type = "FARGATE"
+
+  desired_count = 1
+
+  network_configuration {
+    subnets = aws_subnet.webhook_subnet[*].id
+    security_groups = [aws_security_group.ecs_task_sg.id]
+    assign_public_ip = true
+  }
+}
+
 # Outputs
 output "api_gateway_deployed_url" {
   description = "The URL of the deployed API Gateway"
